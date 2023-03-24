a3793edc125c361fa8f1e60c9bb5eb17ed0fd693
ThrawnGames <113761369+ThrawnGames22@users.noreply.github.com>
2023-03-24 17:01:47 +0800

    Changed Pixel Shader Type and added Smooth Camera Mechanic

--- LL_Project/Lichs Lair/Assets/Code/Camera/CameraSmoothFollow.cs
+++ LL_Project/Lichs Lair/Assets/Code/Camera/CameraSmoothFollow.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CameraSmoothFollow : MonoBehaviour
 {
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    /*
     public Transform target;
     public float cameraSpeed = 15;
     public float zOffset = 22;
     public bool smoothFollow = true;
+    */
+
+    //private Vector3 offset = new Vector3(0f, 0f, -10f);
+    //private float smoothTime = 0.25f;
+    //private Vector3 Velocity = Vector3.zero;
+
+    //[SerializeField] private Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +29,7 @@ public class CameraSmoothFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(target)
         {
             Vector3 newPos = transform.position;
@@ -33,5 +46,17 @@ public class CameraSmoothFollow : MonoBehaviour
                 transform.position = Vector3.Lerp(transform.position, newPos, cameraSpeed * Time.deltaTime);
             }
         }
+        */
+        //Vector3 targetPostion = target.position + offset;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPostion, ref Velocity, smoothTime);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
     }
 }
