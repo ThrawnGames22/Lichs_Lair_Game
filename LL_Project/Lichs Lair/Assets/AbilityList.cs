785a4fa2414cccd815d4bb0110337982baae0e90
ThrawnGames <113761369+ThrawnGames22@users.noreply.github.com>
2023-03-27 13:32:03 +0800

    added base code for spell abilities

---
+++ LL_Project/Lichs Lair/Assets/AbilityList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityList : MonoBehaviour
+{
+  public List<SpellAbility> ability = new List<SpellAbility>();
+}
